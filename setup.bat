3741bd33a24f6b7ad725e47a87cb01b7bcf11ee1
Tejaji-0 <psntejaji@gmail.com>
2025-07-19 13:35:43 +0530

    Update setup.bat
    Signed-off-by: Tejaji-0 <psntejaji@gmail.com>

--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
 @echo off
 echo Setting up Samosa GPT Portable...
 python -m pip install --user -r requirements.txt
+cd streamlit_markdown_select
+cd frontend
+npm install
+npm run build
 echo Setup complete!
 echo.
 echo To run Samosa GPT:
